9c51ac4cc0e2ee695d169a6397a74e679edebb4d
kongtaoxing <18333318658@163.com>
2022-10-27 09:08:37 -0700

    try to add eth_signtyped v4

--- scripts/run.js
+++ scripts/run.js
@@ -50,20 +50,42 @@ const main = async () => {
     const __mintNFT = await nftContract.mint();
     await __mintNFT.wait();
 
-    // const EIP712Domain = [  
-    //     { name: "name", type: "string" },  
-    //     { name: "version", type: "string" },  
-    //     { name: "chainId", type: "uint256" },  
-    //     { name: "verifyingContract", type: "address" },
-    // ];
- 
-    // const { chainId } = library.network;
-    // const _domain = {    
-    //     name: nftContract.address,    
-    //     version: "1",    
-    //     chainId,    
-    //     verifyingContract: nftContract.address 
-    // };
+    const chainId = await guy.getChainId(); // 1337
+    console.log('Chain ID:', chainId);
+    const nonce = 0;
+    const name = "Gold";
+    const version = "1";
+    const token = "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1";
+    const spender = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+    const value = 1000;
+    const deadline = 100;
+
+    const abi = ethers.utils.defaultAbiCoder;
+
+    const _PERMIT_TYPEHASH = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(
+        "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"));
+
+    const structHash = ethers.utils.keccak256(abi.encode(
+        ["bytes32", "address", "address", "uint256", "uint256", "uint256"],
+        [_PERMIT_TYPEHASH, guy.address, spender, value, nonce, deadline]));
+
+    const typeHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(
+        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"));
+    const nameHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(name));
+    const versionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(version));
+    const domainSeparator = ethers.utils.keccak256(abi.encode(
+        ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+        [typeHash, nameHash, versionHash, chainId, token]
+    ));
+
+    const typedDataHash = ethers.utils.keccak256(ethers.utils.solidityPack(
+        ["string", "bytes32", "bytes32"],
+        ["\x19\x01", domainSeparator, structHash]));
+
+    const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+    const signingKey = new ethers.utils.SigningKey(privateKey); // without prefix "\x19Ethereum Signed Message:\n32"
+
+    const signature = await signingKey.signDigest(typedDataHash);
 
     const __list = await marketContract.listNFT(nftContract.address, 3, ethwei.parseEther('1'));
     console.log('listed, price: ', await marketContract.getListPrice(nftContract.address, 3));
