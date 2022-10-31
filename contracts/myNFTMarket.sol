c3a833d138128a9ae584a67a997f24541d977db8
kongtaoxing <18333318658@163.com>
2022-10-31 06:25:51 -0700

    Wow!!

--- contracts/myNFTMarket.sol
+++ contracts/myNFTMarket.sol
@@ -64,7 +64,7 @@ contract NFTMarket {
         }
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
-                keccak256("EIP712Domain(string name, string version, uint256 chainId, address verifyingContract)"),
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                 keccak256(bytes(name)),
                 keccak256(bytes(version)),
                 chainId,
